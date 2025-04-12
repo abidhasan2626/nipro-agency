eb541d973f9e9ed31acb5c040357e3ec63b675b4
Abid Hasan <118461844+abidhasan2626@users.noreply.github.com>
2025-04-12 18:45:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Project Filterable Gallery Isotope jQuery
+
+var $grid = $('#all-project').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
